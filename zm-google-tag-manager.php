1cf405b2a36c658510158bd2fc303d61ddfd5807
Ivan Shim <zeusmicro@gmail.com>
2020-10-28 02:52:50 +0800

    Wed Oct 28 02:52:50 +08 2020

--- zm-google-tag-manager.php
+++ zm-google-tag-manager.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name:		Google Tag Manager Installer
+Plugin Name:		Installer for Google Tag Manager
 Plugin URI:		https://github.com/zeusmicro/zm-google-tag-manager
 Description:		This is a lightweight Wordpress plugin that places the Google Tag Manager code at the appropriate places in the webpage.
 Version:		0.0.91
